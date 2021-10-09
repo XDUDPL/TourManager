e255dc0b426335b82118489163306a4af28469ae
NgocMinhnn <61500935+NgocMinhnn@users.noreply.github.com>
2021-10-09 23:41:32 +0700

    Initial commit

---
+++ src/main/java/com/example/tourmanager/TourManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tourmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tourmanager/TourManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tourmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
